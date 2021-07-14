6b54f258999de09d2b49fe47efdaaec206eb2d00
sofiiadidovych <sofiiadidovych@gmail.com>
2021-07-14 12:36:31 +0200

    Try to deploy on Heroku

--- server/index.js
+++ server/index.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const express = require("express");
 
 const PORT = process.env.PORT || 3001;
@@ -9,6 +10,14 @@ const clientsRouter = require("./api/clients");
 router.use("/clients", clientsRouter);
 
 app.use("/api", router);
+
+// Have Node serve the files for our built React app
+app.use(express.static(path.resolve(__dirname, '../client/build')));
+
+// All other GET requests not handled before will return our React app
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
+});
 
 app.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
